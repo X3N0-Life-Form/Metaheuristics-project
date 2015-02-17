da9d2dc13ad4113e7be876e70a88c6cc28347b2b
X3N0-Life-Form <adrarsis@laposte.net>
2015-02-17 21:00:58 +0100

    boat: getters

--- src/boat.cpp
+++ src/boat.cpp
@@ -7,3 +7,19 @@ Boat::Boat() : number(1), capacity(1), crew_size(1)
 Boat::Boat(int number, int capacity, int crew_size) :
   number(number), capacity(capacity), crew_size(crew_size)
 {}
+
+int Boat::getNumber() {
+  return number;
+}
+
+int Boat::getId() {
+  return number - 1;
+}
+
+int Boat::getCapacity() {
+  return capacity;
+}
+
+int Boat::getCrew_size() {
+  return crew_size;
+}
--- src/boat.h
+++ src/boat.h
@@ -9,6 +9,14 @@ class Boat {
  public:
   Boat();
   Boat(int, int, int);
+
+  int getNumber();
+  /**
+   * = number - 1; useful to know the boat's position in the vector.
+   */
+  int getId();
+  int getCapacity();
+  int getCrew_size();
 };
 
 #endif
