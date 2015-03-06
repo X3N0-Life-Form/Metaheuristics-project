496148b39d55abb03be06bf792811e3d56dbfd66
X3N0-Life-Form <adrarsis@laposte.net>
2015-03-06 08:42:41 +0100

    implemented tabu search

--- src/tabuSearch.hpp
+++ src/tabuSearch.hpp
@@ -3,24 +3,57 @@
 
 #include "localSearch.hpp"
 
-#define SWAP_HISTORY(p, t1, t2) std::pair<int, std::pair<int, int> >(p, std::pair<int, int>(t1, t2))
+class SwapHistory {
+public:
+	int period;
+	int first;
+	int second;
+
+	SwapHistory(int period, int first, int second) :
+		period(period), first(first), second(second)
+	{}
+
+	SwapHistory(const SwapHistory& other) :
+		period(other.period), first(other.first), second(other.second)
+	{}
+};
+
+ostream& operator<<(ostream& out, const SwapHistory& history) {
+	out << "SwapHistory: " << history.period << " [" << history.first << " <=> " << history.second << "]" << endl;
+	return out;
+}
 
 class TabuSearch : public LocalSearch {
 private:
   /**
    * period ==> swap
    */
-  std::list<std::pair<int, std::pair<int, int> > > history_swap;
+  std::list<SwapHistory> history_swap;
+  unsigned int history_max_size;
 public:
-  TabuSearch(Solution& solution) : LocalSearch(solution) {}
+  TabuSearch(Solution& solution) : LocalSearch(solution),
+	  history_max_size(5)
+  {}
 
   virtual Solution& applyHeuristic() {
     return LocalSearch::applyHeuristic();
   }
 
-  virtual bool stop() {
-    return false;
-  }
+	virtual bool stop() {
+		if (LocalSearch::stop()) {
+			if (history_swap.size() == 0) {
+				return true;
+			} else {
+				SwapHistory prev_swap = history_swap.back();
+				history_swap.pop_back();
+				//cout << "do the previous swap move " << prev_swap << endl;
+				solution.moveSwap(prev_swap.period, prev_swap.first, prev_swap.second);
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 
   virtual bool selectionCondition(Solution& current) {
     return current.getCost() <= solution.getCost();
@@ -28,7 +61,10 @@ public:
 
   virtual void validateSwap(int period, int target1, int target2,
 			    Solution& current) {
-    history_swap.push_back(SWAP_HISTORY(period, target1, target2));
+	history_swap.push_back(SwapHistory(period, target1, target2));
+	if (history_swap.size() > history_max_size) {
+		history_swap.pop_front();
+	}
     LocalSearch::validateSwap(period, target1, target2, current);
   }
 
